483946792b0122903e32eeaaf24cb35c403ec814
Stephan Hoyer <ste.hoyer@gmail.com>
2018-10-05 20:41:02 +0200

    Allow arrows at the end of lines

--- components/line.js
+++ components/line.js
@@ -1,13 +1,23 @@
 const m = require('mithril')
 
 const MARGIN = 10
+const AW = 10
+const AH = 20
+
+const px = x => `${x}px`
+
+function distance(a, b) {
+  return Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2))
+}
 
 function render(offsetX, offsetY, scaleX, scaleY, args) {
   args = Object.assign(
     {
-      strokeWidth: '2px',
+      strokeWidth: 2,
       color: 'orange',
       strokeDasharray: null,
+      startForm: null,
+      endForm: null,
     },
     args
   )
@@ -36,20 +46,56 @@ function render(offsetX, offsetY, scaleX, scaleY, args) {
     }
   }
 
-  return [
-    m('path', {
-      d: `M ${[[args.startX, args.startY], [args.endX, args.endY]]
-        .map(p => [scaleX * (offsetX + p[0]), scaleY * (offsetY - p[1])].join())
-        .join(' ')}`,
+  function endFormView() {
+    if (args.endForm === 'arrow') {
+      return m('path', {
+        d: `M ${scaleX * (offsetX + args.endX)},${scaleY *
+          (offsetY - args.endY)}
+        c ${-AW / 8}, ${AH / 2}, ${-AW / 4}, ${AH + -AW / 4}, ${-AW / 2}, ${AH}
+          0, 0, 0, 0, ${AW / 2}, ${-AW / 4}
+          0, 0, 0, 0, ${AW / 2}, ${AW / 4}
+          ${-AW / 4}, ${-AW / 4}, ${(3 * -AW) / 8}, ${-AH / 2}, ${-AW /
+          2}, ${-AH}
+        `,
+        style: {
+          fill: args.color,
+          transformOrigin: `${scaleX * (offsetX + args.endX)}px ${scaleY *
+            (offsetY - args.endY)}px`,
+          transform: `rotate(${Math.atan(
+            (args.endX - args.startX) / (args.endY - args.startY)
+          )}rad)`,
+        },
+      })
+    }
+  }
+
+  function lineView() {
+    let points = [[args.startX, args.startY], [args.endX, args.endY]].map(p => [
+      scaleX * (offsetX + p[0]),
+      scaleY * (offsetY - p[1]),
+    ])
+    if (args.endForm === 'arrow') {
+      const length = distance(points[0], points[1])
+      const factor = (length - AH / 2) / length
+
+      points[1] = [
+        points[0][0] + (points[1][0] - points[0][0]) * factor,
+        points[0][1] + (points[1][1] - points[0][1]) * factor,
+      ]
+    }
+
+    return m('path', {
+      d: `M ${points.map(p => p.join()).join(' ')}`,
 
       style: {
-        strokeWidth: args.strokeWidth,
+        strokeWidth: px(args.strokeWidth),
         stroke: args.color,
         strokeDasharray: args.strokeDasharray,
+        strokeAlignment: 'center',
       },
-    }),
-    labelView(),
-  ]
+    })
+  }
+  return [lineView(), endFormView(), labelView()]
 }
 
 module.exports = {
