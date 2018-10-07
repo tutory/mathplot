1ae49c30c6dcc034f63d5a8c80b84e0702bc7862
Stephan Hoyer <ste.hoyer@gmail.com>
2018-10-07 16:02:08 +0200

    Add label support for polygons

--- components/polygon.js
+++ components/polygon.js
@@ -1,5 +1,9 @@
 const m = require('mithril')
 
+function sum(arr) {
+  return arr.reduce((sum, v) => sum + v, 0)
+}
+
 function render(offsetX, offsetY, scaleX, scaleY, args) {
   args = Object.assign(
     {
@@ -9,16 +13,42 @@ function render(offsetX, offsetY, scaleX, scaleY, args) {
     },
     args
   )
-  return m('path', {
-    d: `M ${args.points
-      .map(p => [scaleX * (offsetX + p[0]), scaleY * (offsetY - p[1])].join())
-      .join(' ')} Z`,
-    style: {
-      strokeWidth: args.strokeWidth,
-      stroke: args.stroke,
-      fill: args.fill,
-    },
-  })
+
+  function pathView() {
+    return m('path', {
+      d: `M ${args.points
+        .map(p => [scaleX * (offsetX + p[0]), scaleY * (offsetY - p[1])].join())
+        .join(' ')} Z`,
+      style: {
+        strokeWidth: args.strokeWidth,
+        stroke: args.stroke,
+        fill: args.fill,
+      },
+    })
+  }
+
+  function labelView() {
+    if (args.label) {
+      const centerX = sum(args.points.map(p => p[0])) / args.points.length
+      const centerY = sum(args.points.map(p => p[1])) / args.points.length
+      return m(
+        'text',
+        {
+          x: scaleX * (offsetX + centerX),
+          y: scaleY * (offsetY - centerY),
+          style: {
+            fill: args.stroke,
+            textAnchor: 'middle',
+            alignmentBaseline: 'middle',
+            stroke: 'none',
+          },
+        },
+        args.label
+      )
+    }
+  }
+
+  return [pathView(), labelView()]
 }
 
 module.exports = {
